e4a36e3de2d2ba5475f227ed1d39babcd2e86429
Anıl Eren Göçer <54250795+anileren5@users.noreply.github.com>
2023-01-22 18:49:47 +0300

    Add files via upload

---
+++ CENG331/Attack Lab/farm.c
@@ -0,0 +1,111 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+void setval_111(unsigned *p)
+{
+    *p = 3599321199U;
+}
+
+void setval_325(unsigned *p)
+{
+    *p = 3285614920U;
+}
+
+unsigned addval_241(unsigned x)
+{
+    return x + 4205398121U;
+}
+
+unsigned getval_389()
+{
+    return 4203300963U;
+}
+
+unsigned getval_170()
+{
+    return 2495742280U;
+}
+
+unsigned addval_301(unsigned x)
+{
+    return x + 3347662924U;
+}
+
+void setval_214(unsigned *p)
+{
+    *p = 180574369U;
+}
+
+void setval_426(unsigned *p)
+{
+    *p = 2432338248U;
+}
+
+void setval_128(unsigned *p)
+{
+    *p = 2428633416U;
+}
+
+void setval_384(unsigned *p)
+{
+    *p = 4205398066U;
+}
+
+unsigned getval_375()
+{
+    return 3599321337U;
+}
+
+unsigned getval_154()
+{
+    return 2428633448U;
+}
+
+void setval_291(unsigned *p)
+{
+    *p = 3281031256U;
+}
+
+void setval_331(unsigned *p)
+{
+    *p = 2428995944U;
+}
+
+void setval_471(unsigned *p)
+{
+    *p = 3347662965U;
+}
+
+unsigned addval_399(unsigned x)
+{
+    return x + 2446756168U;
+}
+
+void setval_464(unsigned *p)
+{
+    *p = 2428633416U;
+}
+
+unsigned addval_362(unsigned x)
+{
+    return x + 2425393368U;
+}
+
+void setval_316(unsigned *p)
+{
+    *p = 2445773128U;
+}
+
+void setval_281(unsigned *p)
+{
+    *p = 2423788635U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
